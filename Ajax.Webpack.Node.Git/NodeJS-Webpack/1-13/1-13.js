748ed8b92eed4d44ce40cd9c31e79b090d22bb98
ManHingHUI <xuwenxing00@gmail.com>
2023-10-18 13:10:00 +0800

    補充

--- Ajax.Webpack.Node.Git/NodeJS-Webpack/1-13/1-13.js
+++ Ajax.Webpack.Node.Git/NodeJS-Webpack/1-13/1-13.js
@@ -1,5 +1,8 @@
 const dayjs = require('dayjs')
 
-const date = dayjs().format('YYYY-MM-DD')
+const date = dayjs().format('YY-MM-DD')
 
 console.log(date);
+//review npm init -y 
+//review npm i (packageName)
+//review npm i             'download all package base on current package.json'
